b1c96be25685a4b72a1910239a5e45fc52ec4df3
Agatha Niwomugizi <niwomugiziagatha@gmail.com>
2025-06-18 03:18:00 +0300

    check role middleware

--- cscms/app/Http/Middleware/CheckRole.php
+++ cscms/app/Http/Middleware/CheckRole.php
@@ -33,7 +33,7 @@ class CheckRole
         if ($request->is('dashboard')) {
             switch ($user->user_type) {
                 case 'farmer':
-                    return redirect()->route('farmers.dashboard');
+                    return redirect()->route('dashboard.farmer');
                 case 'processor':
                     return redirect()->route('processor.dashboard');
                 case 'retailer':
